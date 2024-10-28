9502bd872708eb61bf8c03a3631d6754a046ec2b
sBenicioDev <sjbenicio@hotmail.com>
2024-10-28 10:04:46 -0300

    Definicion estructura de paquetes

--- src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
+++ src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.peluqueriacanina;
 
-/**
- *
- * @author Usuario
- */
 public class PeluqueriaCanina {
 
     public static void main(String[] args) {
